6f59f3114e1a0745cf2e0803cebd00f76ec3b5bf
vic <chen116@usc.edu>
2020-12-08 06:13:59 -0800

    a

--- src/core/coord.rs
+++ src/core/coord.rs
@@ -202,8 +202,8 @@ apps: app::App  )
 
                   },
                   Some("GETWASM") => {
-                  let mut part2s =  (parts.next().unwrap()).splitn(2, ' ');
-                  // let remote_caller = part2s.next().unwrap().to_string(); 
+                  let mut part2s =  (parts.next().unwrap()).splitn(3, ' ');
+                  let remote_caller = part2s.next().unwrap().to_string(); 
 
                       let param = part2s.next().unwrap().to_string().parse::<i32>().unwrap();
                   let wasm_string = part2s.next().unwrap().to_string(); 
@@ -211,7 +211,7 @@ apps: app::App  )
 
 
                   let swasm_bytes =  wasm_string.as_bytes();
-                  println!("wasm byte len:{},from: {}",swasm_bytes.len(),".");
+                  println!("wasm byte len:{},from: {}",swasm_bytes.len(),remote_caller);
                   let store = Store::default();
                       let module = Module::from_binary(store.engine(), swasm_bytes)?;
                       let instance = Instance::new(&store, &module, &[])?;
@@ -226,10 +226,10 @@ apps: app::App  )
 
                       println!("sending fib({}) = {}", param,res );
                       
-                      // let info = format!("RESPONSE {}",res);
-                      // let nb_clone = nb.clone();
-                      // let tx_p = nb_clone.get(&( remote_caller   )).unwrap() ;
-                      // tx_p.send(   info.to_string()).await;
+                      let info = format!("RESPONSE {}",res);
+                      let nb_clone = nb.clone();
+                      let tx_p = nb_clone.get(&( remote_caller   )).unwrap() ;
+                      tx_p.send(   info.to_string()).await;
 
 
 // });
@@ -263,7 +263,7 @@ apps: app::App  )
                           readfile.read_to_end(&mut total_bytes).await;
                           println!("{:?} {}",total_bytes,total_bytes.len() );
                           // victxclone.send(Bytes::copy_from_slice(&total_bytes)).await;
-                          let str_wasm_full = format!("GETWASM {} {}",param,String::from_utf8(total_bytes).unwrap()).to_string();
+                          let str_wasm_full = format!("GETWASM {} {} {}",myaddr,param,String::from_utf8(total_bytes).unwrap()).to_string();
 
                           tx_p.send(str_wasm_full).await;
 
