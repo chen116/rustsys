73ca7e177bf3c0c9de56ea4d57e733baf1cd8632
vic <chen116@usc.edu>
2020-12-15 15:50:56 -0800

    a

--- src/connection/dy_tx.rs
+++ src/connection/dy_tx.rs
@@ -1,20 +1,11 @@
-use tokio::net::{ TcpStream};
-use tokio::stream::{StreamExt};
-use tokio_util::codec::{Framed, LinesCodec};
 
-use futures::SinkExt;
+
 use std::error::Error;
-use std::io;
 
-use bytes::Bytes;
-use futures::{future, Sink, Stream};
-use tokio_util::codec::{BytesCodec, FramedRead, FramedWrite};
 
 use tokio::sync::mpsc;
-use crate::{TX_PORT,RX_PORT};
 use crate::datastore::{neighbour};
 use crate::connection::{tx};
-use tokio::sync::watch;
 
 // use tokio::io::AsyncWriteExt;
 
@@ -25,40 +16,18 @@ pub async fn run(nb: neighbour::Neighbour,dy_tx_c: &mut mpsc::Receiver<String>,)
             println!("dy_tx going to connect with {:?}", remote_host );
             // let addr =  remote_host+":"+RX_PORT;
 
-            let (mut p, mut c) = mpsc::channel(32);
+            let ( p, mut c) = mpsc::channel(32);
             // let clone_host = remote_host.clone();
             nb.set(remote_host.clone(), p);
 
-            let tx = tokio::spawn(async move { 
+            tokio::spawn(async move { 
                 
-                tx::run(remote_host.to_string(),&mut c).await;
+                tx::run(remote_host.to_string(),&mut c).await.expect("could not run tx");
             });
 
-            // handle details
         }
 
-    //   });
 
-
-    // //     let totalk = tokio::spawn(async move { 
-
-    // //         let mut input = String::new();
-    // //         io::stdin().read_line(&mut input).unwrap();
-    // //         input.pop();
-    // //         victxclone.send(input).await;
-
-        
-    // //     });
-    // loop{
-    //         let mut input = String::new();
-    //         io::stdin().read_line(&mut input).unwrap();
-    //         input.pop();
-    //         // let result = stream.write(b"hello world\n").await;
-    //         // let result = stream.write(input).await;
-
-    //         sink.send(input).await.unwrap();
-    //     }
-    // // let done = listen.await?;
 
        
 
